122d3444b5b0724745058f717aa32f5bd47262fd
Levani Sarishvili <levanisarishvili322@gmail.com>
2024-05-16 18:35:23 +0400

    feat: implement page navigation smooth scrolling

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 'use strict';
 
-// =============== Modal window ===============
-
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+// =============== Modal window ===============
 
 const openModal = function (e) {
   e.preventDefault();
@@ -30,12 +32,9 @@ document.addEventListener('keydown', function (e) {
 });
 
 // ============== Smooth scrolling ==============
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
-
+// Button scrolling
 btnScrollTo.addEventListener('click', function (e) {
   const s1coords = section1.getBoundingClientRect();
-  console.log(s1coords);
 
   // scrolling
   // window.scrollTo(
@@ -52,4 +51,29 @@ btnScrollTo.addEventListener('click', function (e) {
 
   // Modern way
   section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+// Page navigation
+// document.querySelectorAll('.nav__link').forEach(el =>
+//   el.addEventListener('click', function (e) {
+//     e.preventDefault();
+
+//     const id = this.getAttribute('href');
+//     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+//   })
+// );
+
+// Event delagation
+
+// 1. Add event listener to common parent element
+// 2. Determine what element originated the event
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+  const id = e.target.getAttribute('href');
+
+  // Matching strategy
+  if (e.target.classList.contains('nav__link')) {
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
 });
