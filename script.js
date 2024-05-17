284a600aab1e48cb93f6085dd6957dc71cd0056a
Levani Sarishvili <levanisarishvili322@gmail.com>
2024-05-17 21:33:39 +0400

    feat: implement tabbed component

--- script.js
+++ script.js
@@ -77,3 +77,26 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
   }
 });
+
+// ============== Tabbed component ==============
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+
+  // Cuard clause
+  if (!clicked) return;
+
+  // Active tab
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(t => t.classList.remove('operations__content--active'));
+
+  clicked.classList.add('operations__tab--active');
+
+  // Activate content are
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
